9e6e81d55bb96ff3899f2098c89f58feff678cc7
duskygloom <sarkarharsh708@gmail.com>
2024-01-19 16:07:21 +0530

    page 4 complete

---
+++ u_vowels.py
@@ -0,0 +1,19 @@
+'''
+Write a program that determines if a given character is a vowel or a consonant. Ask them to
+modify the code to handle both uppercase and lowercase letters.
+c h a r = i n p u t ( ” E n t e r a s i n g l e c h a r a c t e r : ” )
+# Modify t h e c o d e t o h a n d l e both u p p e r c a s e and l o w e r c a s e l e t t e r s
+i f c h a r . l o w e r ( ) i n ’ a e i o u ’ :
+p r i n t ( ‘ Vowel ’ )
+e l s e :
+p r i n t ( ‘ Consonant ’ )
+'''
+
+char = input("Enter a single character: ")
+
+if char.lower() in "aeiou":
+    print("Vowel")
+
+else:
+    print("Consonant")
+    
---
+++ v_square_pattern.py
@@ -0,0 +1,17 @@
+'''
+Write a program that prints a pattern using nested loops to create a triangle of numbers.
+Ask them to modify the code to print a square pattern.
+rows = i n t ( i n p u t ( ” E n t e r t h e number o f rows f o r t h e p a t t e r n : ” ) )
+# Modify t h e c o d e t o p r i n t a d i f f e r e n t p a t t e r n
+f o r i i n r a n g e ( 1 , rows + 1 ) :
+f o r j i n r a n g e ( 1 , i + 1 ) :
+p r i n t ( j , end=” ” )
+p r i n t ( )
+'''
+
+rows = int(input("Enter the number of rows for the pattern: "))
+
+for i in range(1, rows+1):
+    for j in range(1, rows+1):
+        print(j, end=" ")
+    print()
